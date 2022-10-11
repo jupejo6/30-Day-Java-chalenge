1ed5190ce861bd3f2a084f54bd0500222d5d5bf1
jupejo6 <juhasz.peter.jozsef@gmail.com>
2022-10-11 16:41:39 +0200

    _15_30day solution, without review, without Test

--- _15_30day/src/Main.java
+++ _15_30day/src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Main(){
+
+
+
+
+
 }
