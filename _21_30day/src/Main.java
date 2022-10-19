22fc6fc2102dcbbd04815acaa77ba65e81fa2218
jupejo6 <juhasz.peter.jozsef@gmail.com>
2022-10-19 16:08:31 +0200

    _21_30day without tests.

--- _21_30day/src/Main.java
+++ _21_30day/src/Main.java
@@ -1,31 +1,23 @@
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
-
-
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt).toList();
-
-        Integer[] intArray = new Integer[a.size()];
-        intArray = a.toArray(intArray);
-
+        List<Integer> a = new ArrayList<>(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt).toList());
 
         int numberOfSwaps = 0;
+
         for (int i = 0; i < n; i++) {
 
             for (int j = 0; j < n - 1; j++) {
                 // Swap adjacent elements if they are in decreasing order
-                if (intArray[j] > intArray[j + 1]) {
-                    int tempInt = intArray[j];
-                    intArray[j] = intArray[j + 1];
-                    intArray[j + 1] = tempInt;
+                if (a.get(j) > a.get(j + 1)) {
+                    Collections.swap(a, j,j+1);
                     numberOfSwaps++;
                 }
             }
@@ -35,9 +27,11 @@ public class Main {
 
             }
         }
+
+
             System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
-            System.out.println("First Element: " + intArray[0]);
-            System.out.println("Last Element: " + intArray[n - 1]);
+            System.out.println("First Element: " + a.get(0));
+            System.out.println("Last Element: " + a.get(n - 1));
 
             bufferedReader.close();
 
