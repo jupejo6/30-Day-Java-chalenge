c1909b711d3a453505d36da200b9953d95354102
jupejo6 <juhasz.peter.jozsef@gmail.com>
2022-11-03 17:58:40 +0100

    _29_30day without test

--- _29_30day/src/Main.java
+++ _29_30day/src/Main.java
@@ -1,6 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        String myRegExString = "@gmail\\.com$";
+        Pattern p = Pattern.compile(myRegExString);
+        ArrayList<String> validNames = new ArrayList<String>();
 
+        IntStream.range(0, N).forEach(NItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                String firstName = firstMultipleInput[0];
+
+                String emailID = firstMultipleInput[1];
+
+                Matcher m = p.matcher(emailID);
+                if (m.find())
+                    validNames.add(firstName);
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+
+        });
+
+        Collections.sort(validNames);
+        validNames.forEach( s -> System.out.println(s));
+        bufferedReader.close();
     }
 }
+
+
